87c9daee6b3111eb81a65bdbbd884693db3f42c8
takeuti2319 <k024g0068@g.neec.ac.jp>
2025-10-06 16:45:01 +0900

    01_01課題

--- pg3_00_01/main.cpp
+++ pg3_00_01/main.cpp
@@ -1,21 +1,55 @@
 #include <iostream>
 #include <Windows.h>
-#include <stdlib.h>
-#include <vector>
+#include <stdio.h>
+#include <list>
 using namespace std;
 
-
-
-
 int main() {
-	//SetConsoleOutputCP(65001); // UTF-8に設定
-	char str[] = "ソ";
-	printf("%s",str);
 
-	vector<int> b;
+	// 1970年の山手線の駅名を格納する可変長配列
+	list<string> yamanoteLine = {"Tokyo",  "Kannda",    "Akihabara", "Okachimachi",  "Ueno",       "Uguisudani",   "Nippori",   "Tabata",   "Komagome", "Sugamo",
+	                             "Otsuka", "Ikebukuro", "Mejiro",    "Takadanobaba", "Shin-Okubo", "Shinjuku",     "Yoyogi",    "Harajuku", "SHibuya",  "Ebisu",
+	                             "Meguro", "Gotanda",   "Osaki",     "Shinagawa",    "Tamachi",    "Hamamatsucho", "Shimbashi", "Yurakucho"};
 
-	system("pause");
-	return 0;
+	printf("1970年\n"); 
+
+	// 1970年の山手線表示
+	for (const auto& station : yamanoteLine) {
+		printf("%s\n", station.c_str()); // 各駅名を表示
+	}
+
+	printf("\n2019年\n"); 
+
+
+	// 1970年の山手線に新駅を追加
+	for (list<string>::iterator itr = yamanoteLine.begin(); itr != yamanoteLine.end(); ++itr) {
+		if (*itr == "Nippori") {
+			yamanoteLine.insert(++itr, "Nishi-Nippori"); // 田端の次に西日暮里追加
+			break;                                         // 追加後、ループを終了
+		}
+	}
+
+	// 1970年の山手線表示
+	for (const auto& station : yamanoteLine) {
+		printf("%s\n", station.c_str()); // 各駅名を表示
+	}
+
+		printf("\n2022年\n");
+
+	// 1970年の山手線に新駅を追加
+	for (list<string>::iterator itr = yamanoteLine.begin(); itr != yamanoteLine.end(); ++itr) {
+		if (*itr == "Shinagawa") {
+			yamanoteLine.insert(++itr, "Takanawa Gateway"); // 品川の次に高輪ゲートウェイ追加
+			break;                                       // 追加後、ループを終了
+		}
+	}
+
+	// 1970年の山手線表示
+	for (const auto& station : yamanoteLine) {
+		printf("%s\n", station.c_str()); // 各駅名を表示
+	}
+
+
 
 	return 0;
 }
