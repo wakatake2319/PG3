b976f90b9760d4b621d1cebbf15d9c7bda1f6c93
takeuti2319 <k024g0068@g.neec.ac.jp>
2025-10-06 14:41:17 +0900

    01-01課題

--- pg3_00_01/main.cpp
+++ pg3_00_01/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 #include <Windows.h>
+#include <stdlib.h>
+#include <vector>
+using namespace std;
+
 
 
 int main() {
 	//SetConsoleOutputCP(65001); // UTF-8に設定
 	char str[] = "ソ";
 	printf("%s",str);
+
+	vector<int> b;
+
+	system("pause");
+	return 0;
+
 	return 0;
 }
